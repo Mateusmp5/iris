5500c42647f8c499e2a0ec2669b221b0aaebff0b
Mateusmp5 <98540763+Mateusmp5@users.noreply.github.com>
2022-01-31 15:11:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -844,12 +844,9 @@ module.exports = kconfig = async (kill, message) => {
 	
 			
         case 'menu':
-			const theMsg = getMsg(user, msgcount)
-			const uzrXp = getXp(user, nivel)
-			const uzrlvl = getLevel(user, nivel)
-			const uneedxp = 5 * Math.pow(uzrlvl, 2) + 50 * uzrlvl + 100
-			const mping = processTime(t, moment())
-			await kill.sendText(from, mess.menu(pushname, time, theMsg, uzrXp, uneedxp, uzrlvl, mping, patente))
+		const theMsg = getMsg(user, msgcount)
+		const mping = processTime(t, moment())
+		await kill.sendText(from, mess.menu(pushname, time, theMsg, uzrXp, uneedxp, uzrlvl, mping, patente))
             break
 			
 	
